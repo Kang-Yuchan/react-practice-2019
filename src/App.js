11774027fce2ae4e6a1be80b10836a534aeaeffe
Kang.Yuchan <ikimaku0517@gmail.com>
2020-01-01 22:31:34 +0900

    Protection with PropTypes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
 
 function Header() {
@@ -7,34 +8,54 @@ function Header() {
     );
 }
 
-function Food ({fav, nation}) {
-  return <h1>I like {fav}. This is from {nation}</h1>
+function Food ({fav, nation, rating}) {
+  return (
+    <>
+      <h1>I like {fav}. This is from {nation}</h1>
+      <h2>This food's rating is {rating}/5.</h2>
+    </>
+  )
+}
+
+Food.propTypes = {
+  fav: PropTypes.string.isRequired,
+  nation: PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
 }
 
 const favFoods = [
   
     {
+      id: 1,
       name: "Kimchi",
-      nation: "Korea"
+      nation: "Korea",
+      rating: 5
     },
     {
+      id: 2,
       name: "Sushi",
-      nation: "Japan"
+      nation: "Japan",
+      rating: 5
     },
     {
+      id: 3,
       name: "Pasta",
-      nation: "Italy"
+      nation: "Italy",
+      rating: 4
     }
   ]
-
+  
 
 function App() {
 	return (
 		<>
       <Header />
-			<p>Hello react!</p>
-      {favFoods.map(food => <Food fav={food.name} nation={food.nation}/>)}
-		</>
+      {favFoods.map(dish => <Food 
+      key={dish.id} 
+      fav={dish.name} 
+      nation= {dish.nation} 
+      rating= {dish.rating} />)}
+    </>
 	);
 }
 
