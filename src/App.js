343dbcbcfc18cfb92eae2fd3dc83744f2c7f60dc
Kang.Yuchan <ikimaku0517@gmail.com>
2020-01-05 09:38:44 +0900

    Styling Movies

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import axios from "axios";
+import axios from 'axios';
+import Movie from './Movie';
+import './App.css';
 
 class App extends React.Component {
 	state = {
-    isLoading: true,
-    movies: []
-  };
+		isLoading: true,
+		movies: []
+	};
 
-  getMovies = async () => {
-    const movies = await axios.get("https://yts-proxy.now.sh/movie_details.json");
-  }
+	getMovies = async () => {
+		const { data: { data: { movies } } } = await axios.get(
+			'https://yts-proxy.now.sh/list_movies.json?sort_by=rating'
+		);
+		this.setState({ movies, isLoading: false });
+	};
 
-  componentDidMount() {
-    this.getMovies();
-  }
+	componentDidMount() {
+		this.getMovies();
+	}
 	render() {
-    const { isLoading } = this.state;
-    return (
-      <>
-        <div>{isLoading ? "I'm loading" : "Ready Perfectly"}</div>
-      </>
-    )}
-  }
-  
-  export default App;
+		const { isLoading, movies } = this.state;
+		return (
+			<section className="container">
+				{isLoading ? (
+					<div className="loader">
+						<span className="loader_text">Loading...</span>
+					</div>
+				) : (
+					<div className="movies">
+						{movies.map((movie) => {
+							return (
+								<Movie
+									key={movie.id}
+									id={movie.id}
+									year={movie.year}
+									title={movie.title}
+									summary={movie.summary}
+									poster={movie.medium_cover_image}
+									genres={movie.genres}
+								/>
+							);
+						})}
+					</div>
+				)}
+			</section>
+		);
+	}
+}
+
+export default App;
