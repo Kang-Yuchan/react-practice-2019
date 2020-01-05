8feda097cc7da32f0a73af22449f2b11ee19ef1a
Kang.Yuchan <ikimaku0517@gmail.com>
2020-01-05 14:00:41 +0900

    Adding Detail and Styling Detail

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -1,8 +1,37 @@
 import React from 'react';
-
-function Detail(props) {
-	console.log(props);
-	return <span>Hello</span>;
+import './Detail.css';
+class Detail extends React.Component {
+	componentDidMount() {
+		const { location, history } = this.props;
+		if (location.state === undefined) {
+			history.push('/');
+		}
+	}
+	render() {
+		const { location } = this.props;
+		console.log(location.state);
+		return (
+			<div className="detail">
+				<div className="detail__img">
+					<img src={location.state.poster} alt="title" title={location.state.title} />
+				</div>
+				<div className="detail__data">
+					<h3 className="detail__title">{location.state.title}</h3>
+					<h5 className="detail__year">{location.state.year}</h5>
+					<ul className="detail__genres">
+						{location.state.genres.map((genre, index) => (
+							<li key={index} className="genres__genre">
+								{genre}
+							</li>
+						))}
+					</ul>
+					<div className="detail__summary">
+						<p>{location.state.summary}</p>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default Detail;
